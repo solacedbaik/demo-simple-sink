5fc1a1b731e85199256e88181d45e9c4021cb885
solacedbaik <dale.baik@solace.com>
2018-08-09 13:37:07 -0700

    Removed unused imports

--- spring-cloud-starter-stream-sink-mylog/src/main/java/org/springframework/cloud/stream/app/mylog/sink/MylogSinkConfiguration.java
+++ spring-cloud-starter-stream-sink-mylog/src/main/java/org/springframework/cloud/stream/app/mylog/sink/MylogSinkConfiguration.java
@@ -8,16 +8,10 @@ package org.springframework.cloud.stream.app.mylog.sink;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.integration.handler.LoggingHandler;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.context.annotation.Bean;
-import java.util.logging.Level;
-
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.SignalType;
 
 /**
  * A simple logger sink
